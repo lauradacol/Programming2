4a239519ae24316f75f3102b81336b058bd2577f
aluno-uffs <Aluno UFFS>
2018-09-17 15:37:20 -0300

    prova

---
+++ Prova/prova.js
@@ -0,0 +1,9 @@
+document.getElementById("valor").onchange = function(){
+	
+	var total = document.getElementsByClassName("total")[0]
+	
+	document.getElementById('total').innerHTML= getElementById("valor")+getElementById("total");
+}
+
+
+
