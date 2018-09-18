a576157a5c66877a1a83b1b5f1eb482acec3ecb8
Laura Dacol <laura.dacol@gmail.com>
2018-09-17 22:08:40 -0300

    prova

--- Prova/prova.js
+++ Prova/prova.js
@@ -1,9 +1,13 @@
-document.getElementById("valor").onchange = function(){
+function changeValor(){
+	var valor = parseFloat(document.getElementById("valor").value);
+	var total = parseFloat(document.getElementById("total").innerHTML);
 	
-	var total = document.getElementsByClassName("total")[0]
-	
-	document.getElementById('total').innerHTML= getElementById("valor")+getElementById("total");
+	document.getElementById("total").innerHTML = total + valor;
 }
 
-
-
+function changeDesc(){
+	var desc = parseFloat(document.getElementById("desc").value);
+	var total = parseFloat(document.getElementById("total").innerHTML);
+	
+	document.getElementById("total").innerHTML = total - desc;
+}
