5dfa1d5b2d14ad894552cad8c8e30740a7d61ec3
Laura Dacol <laura.dacol@gmail.com>
2018-09-20 23:01:43 -0300

    colecao

--- Prova/prova.js
+++ Prova/prova.js
@@ -11,3 +11,45 @@ function changeDesc(){
 	
 	document.getElementById("total").innerHTML = total - desc;
 }
+
+function nomeEmpty(){
+	if (document.getElementById("nome").value == ""){
+		return "Preencher nome!\n";
+		/*alert("Preencher campo nome!");		*/
+	}	
+	else{
+		return "";
+	}
+}
+
+function descricaoEmpty(){
+	if(document.getElementById("descricao").value == ""){
+		return "Preencher descrição!\n";	
+	}
+	else{
+		return "";
+	}	
+}
+
+function quantidadeEmpty(){
+	if(valor = document.getElementById("quantidade").value){
+		return false;
+	}	
+}
+
+function valorEmpty(){
+	if(valor = document.getElementById("valor").value){
+		return false;
+	}
+}
+
+function descEmpty(){
+	if(valor = document.getElementById("desc").value){
+		return false;
+	}
+}
+
+function isEmpty(){
+	alert(nomeEmpty() + descricaoEmpty());
+	
+}
