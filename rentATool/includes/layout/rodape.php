de44d1a5064a4dccb0ea90b4f265a16f7d0ad2bb
aluno-uffs <aluno-uffs@lab-comp.cco.uffs.edu.br>
2018-10-01 15:44:33 -0300

    rent a tool

---
+++ rentATool/includes/layout/rodape.php
@@ -0,0 +1,13 @@
+	<!-- rodape -->
+	<footer><p>Rent a Tool - Chapecó/SC</p>
+		<ul class="social">
+			<li><a href="http://facebook.com/rentatool"><img src="img/facebook.png" alt="Facebook"></a></li>
+			<li><a href="http://twitter.com/rentatool"><img src="img/twitter.png" alt="Twitter"></a></li>
+			<li><a href="http://plus.google.com/rentatool"><img src="img/googleplus.png" alt="Google+"></a></li>
+		</ul>
+		<div id="ajuda">Precisa de ajuda? <span id="fechar">x</span></div>
+	</footer>
+	<!-- fim rodape -->
+	<script src="js/functions.js"></script>
+</body>
+</html>
--- rentATool/index.php
+++ rentATool/index.php
@@ -1,56 +1,13 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta charset="utf-8">
-	<title>Rent a Tool</title>
-	<link rel="stylesheet" type="text/css" href="css/rent.css">
-	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> <!-- web font Lobster -->
-</head>
-<body>
-	<!-- cabeçalho -->
-	<header>
-		<h1>Rent a Tool</h1>
-		<p class="carrinho"><a href="#">Meu carrinho <img src="img/cart.png" width="32"></a></p>
-		<p id="exibeMenu">Menu</p>
-		<nav class="menu-opcoes">    
-			<ul>
-				<li><a href="#">Minha Conta</a></li>
-				<li><a href="#">Programa de pontos</a></li>
-				<li><a href="#">Consumo solidário</a></li>
-				<li><a href="#">Quem somos</a></li>
-				<li><a href="#">Ajuda</a></li>
-			</ul>
-		</nav>
-	</header>
-	<!-- fim cabeçalho -->
-
+<?php
+include "includes/layout/cabecalho.php";
+?>
 	<!-- area central com 3 colunas -->
 	<div class="container">
 		
-		<section class="col-1">
-			<section class="busca">
-				<form>
-					<input type="search" placeholder="Busca..." name="busca">
-					<button>OK</button>
-				</form>
-			</section>
-
-			<section class="menu-categorias">
-				<h2>Categorias</h2>
-				<nav>
-					<ul>
-						<?php
-						include "includes/categorias.php";
-						
-						foreach ($CATEGORIAS as $indice => $nomeCategoria){
-							echo "<li><a href='index.php?secao=$indice'>$nomeCategoria</a></li>";
-						}
-						?>
-				</nav>
-			</section>
-		</section>
-
+		<?php
+			include "includes/layout/menuLateral.php";
+		?>
+		
 		<section class="col-2">
 			<?php
 			if(isset($_GET['secao'])){
@@ -95,7 +52,7 @@
 					while ($produto = mysqli_fetch_array($resultado)){
 				?>
 						<div class="produto">
-						  <a href="produto.html">
+						  <a href="produto.php?id=<?=$produto['id'];?>">
 							<figure>
 								<img src="img/produtos/<?=mostraImagem($produto['imagem']);?>" alt="<?=$produto['nome'];?>">
 								<figcaption><?=$produto['nome'];?><span class="preco">
@@ -110,38 +67,14 @@
 				
 			</div>			
 		</section>
+	
+	<?php
+	include "includes/layout/maisPedidos.php";
+	?>	
 
-		<aside class="col-3">
-			<h2>Mais pedidos</h2>
-			<!-- container de mais pedidos -->
-			<div class="lista-produtos">
-				<!-- um produto -->
-				<div class="produto">
-					<a href="produto.html">
-						<figure>
-							<img src="img/produtos/serra.jpg" alt="miniatura1">
-							<figcaption>Serra elétrica<span class="preco">R$ 20,00</span></figcaption>
-						</figure>
-					</a>
-				</div>  
-				<!-- fim produto -->
-				
-				<!-- adicione mais produtos --> 				
-			</div>
-		</aside>
-	</div>
+
 	<!-- fim area central -->
 
-	<!-- rodape -->
-	<footer><p>Rent a Tool - Chapecó/SC</p>
-		<ul class="social">
-			<li><a href="http://facebook.com/rentatool"><img src="img/facebook.png" alt="Facebook"></a></li>
-			<li><a href="http://twitter.com/rentatool"><img src="img/twitter.png" alt="Twitter"></a></li>
-			<li><a href="http://plus.google.com/rentatool"><img src="img/googleplus.png" alt="Google+"></a></li>
-		</ul>
-		<div id="ajuda">Precisa de ajuda? <span id="fechar">x</span></div>
-	</footer>
-	<!-- fim rodape -->
-	<script src="js/functions.js"></script>
-</body>
-</html>
+<?php
+include "includes/layout/rodape.php";
+?>	
