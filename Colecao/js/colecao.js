9141baf252832fc5650563ae397e3c1a3d3ee54b
Laura Dacol <laura.dacol@gmail.com>
2018-10-01 21:18:35 -0300

    colecao

--- Colecao/js/colecao.js
+++ Colecao/js/colecao.js
@@ -9,3 +9,8 @@ function displayDate(){
 	
 	document.getElementById("acesso").innerHTML = "Último acesso em:" +  acesso;
 }
+
+function insertFamilia(){
+	var fam = document.getElementById("nome_fam").value;
+	document.getElementById("tab_fam").innerHTML = fam;	
+}
